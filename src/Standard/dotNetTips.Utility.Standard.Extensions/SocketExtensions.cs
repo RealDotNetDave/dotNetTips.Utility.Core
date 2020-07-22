94ec692709542e8b9bd36f0032934be948cf639d
David McCarter <dotnetdave@live.com>
2020-07-22 10:46:29 -0700

    Added SocketsHelper and SocketExtensions.

---
+++ src/Standard/dotNetTips.Utility.Standard.Extensions/SocketExtensions.cs
@@ -0,0 +1,90 @@
+﻿// ***********************************************************************
+// Assembly         : dotNetTips.Utility.Standard.Extensions
+// Author           : David McCarter
+// Created          : 07-22-2020
+//
+// Last Modified By : David McCarter
+// Last Modified On : 07-22-2020
+// ***********************************************************************
+// <copyright file="SocketExtensions.cs" company="dotNetTips.com - David McCarter">
+//     McCarter Consulting (David McCarter)
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace dotNetTips.Utility.Standard.Extensions
+{
+    /// <summary>
+    /// Extensions for Socket.
+    /// </summary>
+    public static class SocketExtensions
+    {
+
+        /// <summary>
+        /// Binds to an IP address and OS-assigned port. Returns the chosen port.
+        /// </summary>
+        /// <param name="socket">The socket.</param>
+        /// <param name="address">The address.</param>
+        /// <returns>System.Int32.</returns>
+        /// <remarks>NEW: From .NET Core source.</remarks>
+        public static int BindToAnonymousPort(this Socket socket, IPAddress address)
+        {
+            socket.Bind(new IPEndPoint(address, 0));
+
+            return ((IPEndPoint)socket.LocalEndPoint).Port;
+        }
+
+
+        /// <summary>
+        /// On non-Windows platforms, once non-blocking is turned on (either explicitly
+        /// or by performing an async operation), always stay in non-blocking mode.
+        /// </summary>
+        /// <param name="socket">The socket.</param>
+        /// <param name="force">if set to <c>true</c> [force].</param>
+        /// <remarks>NEW: From .NET Core source.</remarks>
+        public static void ForceNonBlocking(this Socket socket, bool force)
+        {
+            if (force)
+            {
+                socket.Blocking = false;
+                socket.Blocking = true;
+            }
+        }
+
+        /// <summary>
+        /// Tries to connect within the provided timeout interval
+        /// Useful to speed up "can not connect" assertions on Windows
+        /// </summary>
+        /// <param name="socket">The socket.</param>
+        /// <param name="remoteEndpoint">The remote endpoint.</param>
+        /// <param name="millisecondsTimeout">The milliseconds timeout.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+        /// <remarks>NEW: From .NET Core source.</remarks>
+        public static bool TryConnect(this Socket socket, EndPoint remoteEndpoint, int millisecondsTimeout)
+        {
+            using (var mre = new ManualResetEventSlim(false))
+            {
+                using (var sea = new SocketAsyncEventArgs() { RemoteEndPoint = remoteEndpoint, UserToken = mre })
+                {
+                    sea.Completed += (s, e) => ((ManualResetEventSlim)e.UserToken).Set();
+
+                    var pending = socket.ConnectAsync(sea);
+
+                    if (!pending || mre.Wait(millisecondsTimeout))
+                    {
+                        return sea.SocketError == SocketError.Success;
+                    }
+
+                    Socket.CancelConnectAsync(sea); // this will close the socket!
+
+                    // In case of time-out, ManualResetEventSlim is left undisposed to avoid race conditions,
+                    // letting SafeHandle's finalizer to do the cleanup.
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ src/Standard/dotNetTips.Utility.Standard/Net/SocketsHelper.cs
@@ -0,0 +1,40 @@
+﻿// ***********************************************************************
+// Assembly         : dotNetTips.Utility.Standard
+// Author           : David McCarter
+// Created          : 07-22-2020
+//
+// Last Modified By : David McCarter
+// Last Modified On : 07-22-2020
+// ***********************************************************************
+// <copyright file="SocketsHelper.cs" company="dotNetTips.com - David McCarter">
+//     McCarter Consulting (David McCarter)
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System.Net;
+using System.Net.Sockets;
+
+namespace dotNetTips.Utility.Standard.Net
+{
+    /// <summary>
+    /// Helper methods for Sockets.
+    /// </summary>
+    public static class SocketsHelper
+    {
+        /// <summary>
+        /// Creates the and start TCP listener on anonymous port. Binds to an OS-assigned port.
+        /// </summary>
+        /// <returns>System.ValueTuple&lt;TcpListener, System.Int32&gt;.</returns>
+        /// <remarks>NEW</remarks>
+        public static (TcpListener listener, int port) CreateAndStartTcpListenerOnAnonymousPort()
+        {
+            var listener = new TcpListener(IPAddress.IPv6Any, 0);
+            listener.Server.DualMode = true;
+
+            listener.Start();
+            var port = ((IPEndPoint)listener.LocalEndpoint).Port;
+
+            return (listener, port);
+        }
+    }
+}
