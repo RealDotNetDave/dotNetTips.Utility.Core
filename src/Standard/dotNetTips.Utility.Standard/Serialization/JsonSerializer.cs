e7c04d3a017224df070a99e19ab9ba9616e7e83d
David McCarter <dotnetdave@live.com>
2020-07-22 10:20:36 -0700

    Added JsonEqual to JsonSerializer.

--- src/Standard/dotNetTips.Utility.Standard/Serialization/JsonSerializer.cs
+++ src/Standard/dotNetTips.Utility.Standard/Serialization/JsonSerializer.cs
@@ -4,18 +4,20 @@
 // Created          : 06-26-2017
 //
 // Last Modified By : David McCarter
-// Last Modified On : 12-02-2019
+// Last Modified On : 07-22-2020
 // ***********************************************************************
 // <copyright file="JsonSerializer.cs" company="dotNetTips.com - David McCarter">
 //     McCarter Consulting (David McCarter)
 // </copyright>
 // <summary></summary>
 // ***********************************************************************
-using dotNetTips.Utility.Standard.OOP;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Json;
 using System.Text;
+using System.Text.Json;
+using dotNetTips.Utility.Standard.OOP;
 
 namespace dotNetTips.Utility.Standard.Serialization
 {
@@ -67,6 +69,87 @@ namespace dotNetTips.Utility.Standard.Serialization
             }
 
             return json;
+        }
+
+        /// <summary>
+        /// Jsons the equal.
+        /// </summary>
+        /// <param name="actual">The actual.</param>
+        /// <param name="expected">The expected.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+        /// <remarks>NEW</remarks>
+        public static bool JsonEqual(string actual, string expected)
+        {
+            using (var expectedDom = JsonDocument.Parse(expected))
+            {
+                using (var actualDom = JsonDocument.Parse(actual))
+                {
+                    return JsonEqual(expectedDom.RootElement, actualDom.RootElement);
+                }
+            }
+        }
+
+        private static bool JsonEqual(JsonElement expected, JsonElement actual)
+        {
+            JsonValueKind valueKind = expected.ValueKind;
+
+            if (valueKind != actual.ValueKind)
+            {
+                return false;
+            }
+
+            switch (valueKind)
+            {
+                case JsonValueKind.Object:
+                    var propertyNames = new HashSet<string>();
+
+                    foreach (var property in expected.EnumerateObject())
+                    {
+                        propertyNames.Add(property.Name);
+                    }
+
+                    foreach (var property in actual.EnumerateObject())
+                    {
+                        propertyNames.Add(property.Name);
+                    }
+
+                    foreach (var name in propertyNames)
+                    {
+                        if (!JsonEqual(expected.GetProperty(name), actual.GetProperty(name)))
+                        {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                case JsonValueKind.Array:
+                    JsonElement.ArrayEnumerator expectedEnumerator = actual.EnumerateArray();
+                    JsonElement.ArrayEnumerator actualEnumerator = expected.EnumerateArray();
+
+                    while (expectedEnumerator.MoveNext())
+                    {
+                        if (!actualEnumerator.MoveNext())
+                        {
+                            return false;
+                        }
+
+                        if (!JsonEqual(expectedEnumerator.Current, actualEnumerator.Current))
+                        {
+                            return false;
+                        }
+                    }
+
+                    return !actualEnumerator.MoveNext();
+                case JsonValueKind.String:
+                    return expected.GetString() == actual.GetString();
+                case JsonValueKind.Number:
+                case JsonValueKind.True:
+                case JsonValueKind.False:
+                case JsonValueKind.Null:
+                    return expected.GetRawText() == actual.GetRawText();
+                default:
+                    throw new NotSupportedException($"Unexpected JsonValueKind: JsonValueKind.{valueKind}.");
+            }
         }
     }
 }
